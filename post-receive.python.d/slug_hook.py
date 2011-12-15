e8a6fe85eb536d80c2e352e88a886bd7ffed5940
Kacper Kornet <draenog@pld-linux.org>
2011-12-15 15:21:35 +0000

    Move code generating files for slug_watch to hook plugin

---
+++ post-receive.python.d/slug_hook.py
@@ -0,0 +1,18 @@
+import os
+import tempfile
+
+from git_slug.serverconst import WATCHDIR
+
+
+def run(data):
+    gitrepo = os.environ.get('GL_REPO')
+    if gitrepo.startswith('packages/'):
+        gitrepo = gitrepo[len('packages/'):]
+    else:
+        return
+
+    (tfile, name) = tempfile.mkstemp(prefix=gitrepo+'.', dir=WATCHDIR)
+    os.write(tfile,(os.getenv('GL_USER')+'\n').encode('utf-8'))
+    os.write(tfile,(gitrepo+'\n').encode('utf-8'))
+    os.write(tfile,''.join(data).encode('utf-8'))
+    os.close(tfile)
