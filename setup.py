60658e341c1b76521c4f6c2ef98d801d23860de2
Kacper Kornet <draenog@pld-linux.org>
2011-12-19 05:49:24 +0000

    ADC to remove a repository

--- setup.py
+++ setup.py
@@ -10,5 +10,6 @@ setup(name='git-core-slug',
       url='https://github.com/draenog/slug',
       classifiers=['Programming Language :: Python :: 3'],
       packages=['git_slug'],
+      data_files=[('/home/services/gitolite/adc/bin', ['trash'])],
       scripts=['slug.py', 'slug_watch']
      )
