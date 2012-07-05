2d97cff809301f030942a3ea5777bfbd18760258
Kacper Kornet <draenog@pld-linux.org>
2012-07-05 19:21:25 +0100

    v0.13

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='git-core-slug',
-      version='0.7',
+      version='0.13',
       description='Scripts to interact with PLD git repos',
       author='Kacper Kornet',
       author_email='draenog@pld-linux.org',
