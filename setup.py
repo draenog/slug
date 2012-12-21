264dc550951ac79fd5761f6ac4792c0ef2c4a81f
Kacper Kornet <draenog@pld-linux.org>
2012-12-21 01:45:03 +0000

    setup.py: Install link copy -> move in adc/bin

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
 
 from distutils.core import setup
+from distutils.command.install_data import install_data
+import os
+
+class post_install(install_data):
+    def run(self):
+        super().run()
+        dirfd =os.open(os.path.join(self.install_dir, self.data_files[0][0]), os.O_RDONLY)
+        os.symlink('move', 'copy', dir_fd=dirfd)
+        os.close(dirfd)
+
 
 setup(name='git-core-slug',
       version='0.13',
@@ -11,5 +21,6 @@ setup(name='git-core-slug',
       classifiers=['Programming Language :: Python :: 3'],
       packages=['git_slug', 'Daemon'],
       data_files=[('adc/bin', ['adc/trash', 'adc/move'])],
-      scripts=['slug.py', 'slug_watch']
+      scripts=['slug.py', 'slug_watch'],
+      cmdclass={"install_data": post_install}
      )
